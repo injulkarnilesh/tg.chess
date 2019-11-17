54f9ce740659de9e973f3cc3cb83d429980a5c0b
Injulkar, Nilesh <nilesh.injulkar@cdk.com>
2019-11-17 13:13:53 +0530

    Chess problem solved

---
+++ src/main/java/tg/chess/Chess.java
@@ -0,0 +1,232 @@
+package tg.chess;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Chess {
+
+    public static Chess createNew() {
+        return new Chess();
+    }
+
+    public List<String> findAllNextPositions(final Piece piece, final String currentPosition) {
+        final Position position = Position.parse(currentPosition);
+        return piece.getPossibleMoves()
+                .stream()
+                .map(move -> move.perform(position))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(p -> p.toString())
+                .collect(Collectors.toList());
+    }
+
+    public enum  Piece {
+        KING(Move.LEFT, Move.RIGHT, Move.UP, Move.DOWN, Move.UP_LEFT,
+                Move.UP_RIGHT, Move.DOWN_LEFT, Move.DOWN_RIGHT),
+
+        PAWN(Move.UP),
+
+        HORSE(Move.UP_LEFT.then(Move.UP), Move.UP_LEFT.then(Move.LEFT),
+                Move.UP_RIGHT.then(Move.UP), Move.UP_RIGHT.then(Move.RIGHT),
+                Move.DOWN_LEFT.then(Move.DOWN), Move.DOWN_LEFT.then(Move.LEFT),
+                Move.DOWN_RIGHT.then(Move.DOWN), Move.DOWN_RIGHT.then(Move.RIGHT)),
+
+        ROOK(Move.UP.keepGoing(), Move.DOWN.keepGoing(),
+                Move.LEFT.keepGoing(), Move.RIGHT.keepGoing()),
+
+        BISHOP(Move.UP_LEFT.keepGoing(), Move.UP_RIGHT.keepGoing(),
+                Move.DOWN_LEFT.keepGoing(), Move.DOWN_RIGHT.keepGoing()),
+
+        QUEEN(Move.UP.keepGoing(), Move.DOWN.keepGoing(), Move.LEFT.keepGoing(), Move.RIGHT.keepGoing(),
+                Move.UP_LEFT.keepGoing(), Move.UP_RIGHT.keepGoing(),
+                Move.DOWN_LEFT.keepGoing(), Move.DOWN_RIGHT.keepGoing());
+
+
+        private List<Move> possibleMoves;
+
+        Piece(final Move ...possibleMoves) {
+            this.possibleMoves = Arrays.asList(possibleMoves);
+        }
+
+        public List<Move> getPossibleMoves() {
+            return possibleMoves;
+        }
+    }
+
+    static class Move {
+
+        private static final Move LEFT = new Move(Position::canMoveLeft, Position::moveLeft);
+        private static final Move RIGHT = new Move(Position::canMoveRight, Position::moveRight);
+        private static final Move UP = new Move(Position::canMoveUp, Position::moveUp);
+        private static final Move DOWN = new Move(Position::canMoveDown, Position::moveDown);
+        private static final Move UP_LEFT = new Move(Position::canMoveUpLeft, Position::moveUpLeft);
+        private static final Move UP_RIGHT = new Move(Position::canMoveUpRight, Position::moveUpRight);
+        private static final Move DOWN_LEFT = new Move(Position::canMoveDownLeft, Position::moveDownLeft);
+        private static final Move DOWN_RIGHT = new Move(Position::canMoveDownRight, Position::moveDownRight);
+
+        private Predicate<Position> canMove;
+        private Function<Position, Position> move;
+
+        public Move(final Predicate<Position> canMove, final Function<Position, Position> move) {
+            this.canMove = canMove;
+            this.move = move;
+        }
+
+        public Optional<Position> perform(final Position currentPosition) {
+            if (canMove.test(currentPosition)) {
+                return Optional.of(move.apply(currentPosition));
+            }
+            return Optional.empty();
+        }
+
+        public Move keepGoing() {
+            final Predicate<Position> andPredicate = (p) ->  {
+                Position temp = p;
+                while (canMove.test(temp)) {
+                    temp = this.move.apply(temp);
+                }
+                return !p.equals(temp);
+            };
+            final Function<Position, Position> andMove = (p) -> {
+                Position temp = p;
+                while (canMove.test(temp)) {
+                    temp = this.move.apply(temp);
+                }
+                return temp;
+            };
+            return new Move(andPredicate, andMove);
+        }
+
+        public Move then(final Move anotherMove) {
+            final Predicate<Position> andPredicate = (p) -> canMove.test(p) && anotherMove.canMove.test(p);
+            final Function<Position, Position> andMove = (p) -> anotherMove.move.apply(this.move.apply(p));
+            return new Move(andPredicate, andMove);
+        }
+    }
+
+    static class Position {
+
+        private static int A_ASCII = (int) 'A';
+        private static int H_ASCII = (int) 'H';
+
+        private char column;
+        private int row;
+
+        private Position(final char column, final int row) {
+            this.column = column;
+            this.row = row;
+        }
+
+        public static Position parse(final String stringPosition) {
+            if (stringPosition == null || stringPosition.length() != 2) {
+                throw new IllegalArgumentException("Incorrect Position " + stringPosition);
+            }
+
+            final char column = stringPosition.charAt(0);
+            final char row = stringPosition.charAt(1);
+            try {
+                final int rowNumber = Integer.parseInt(String.valueOf(row));
+
+                if (column < A_ASCII || column > H_ASCII || rowNumber < 1 || rowNumber > 8) {
+                    throw new IllegalArgumentException("Incorrect Position " + stringPosition + " out of range");
+                }
+
+                return new Position(column, rowNumber);
+            } catch (final NumberFormatException nfe) {
+                throw new IllegalArgumentException("Incorrect Position " + stringPosition, nfe);
+            }
+        }
+
+        boolean canMoveLeft() {
+            return column > A_ASCII;
+        }
+
+        Position moveLeft() {
+            return new Position((char) (column - 1), row);
+        }
+
+        Position moveRight() {
+            return new Position((char) (column + 1), row);
+        }
+
+        Position moveUp() {
+            return new Position(column, row + 1);
+        }
+
+        Position moveDown() {
+            return new Position(column, row - 1);
+        }
+
+        Position moveUpLeft() {
+            return this.moveUp().moveLeft();
+        }
+
+        Position moveUpRight() {
+            return this.moveUp().moveRight();
+        }
+
+        Position moveDownLeft() {
+            return this.moveDown().moveLeft();
+        }
+
+        Position moveDownRight() {
+            return this.moveDown().moveRight();
+        }
+
+        boolean canMoveRight() {
+            return column < H_ASCII;
+        }
+
+        boolean canMoveUp() {
+            return row < 8;
+        }
+
+        boolean canMoveDown() {
+            return row > 1;
+        }
+
+        boolean canMoveUpLeft() {
+            return canMoveLeft() && canMoveUp();
+        }
+
+        boolean canMoveUpRight() {
+            return canMoveUp() && canMoveRight();
+        }
+
+        boolean canMoveDownLeft() {
+            return canMoveDown() && canMoveLeft();
+        }
+
+        boolean canMoveDownRight() {
+            return canMoveDown() && canMoveRight();
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(column) + row;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            final Position position = (Position) o;
+            return column == position.column &&
+                    row == position.row;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(column, row);
+        }
+    }
+
+}
+
+
+
---
+++ src/test/java/tg/chess/ChessTest.java
@@ -0,0 +1,159 @@
+package tg.chess;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ChessTest {
+
+    private Chess chess = Chess.createNew();
+
+    @Test
+    public void kingShouldMoveInAll8DirectionsByOneStep() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.KING, "D5");
+
+        assertThat(nextPositions, hasSize(8));
+        assertThat(nextPositions, hasItems("C6", "D6", "E6", "E5", "E4", "D4", "C4", "C5"));
+    }
+
+    @Test
+    public void kingShouldMoveInAvailableDirectionsFromCorner() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.KING, "A8");
+
+        assertThat(nextPositions, hasSize(3));
+        assertThat(nextPositions, hasItems("B8", "B7", "A7"));
+    }
+
+    @Test
+    public void kingShouldMoveInAvailableDirectionsFromCloseToBorder() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.KING, "H4");
+
+        assertThat(nextPositions, hasSize(5));
+        assertThat(nextPositions, hasItems("H5", "G5", "G4", "G3", "H3"));
+    }
+
+    @Test
+    public void pawnShouldMoveVerticallyUp() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.PAWN, "D4");
+
+        assertThat(nextPositions, hasSize(1));
+        assertThat(nextPositions, hasItems("D5"));
+    }
+
+    @Test
+    public void pawnShouldNotMoveVerticallyUpWhenAtTheEnd() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.PAWN, "D8");
+
+        assertThat(nextPositions, is(empty()));
+    }
+
+    @Test
+    public void horseShouldGo2AndHalfPositionsIn8Directions() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.HORSE, "E3");
+
+        assertThat(nextPositions, hasSize(8));
+        assertThat(nextPositions, hasItems("D5", "F5", "G4", "G2", "F1", "D1", "C2", "C4"));
+    }
+
+    @Test
+    public void horseShouldGo2AndHalfPositionsIn8DirectionsIfPossible() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.HORSE, "A4");
+
+        assertThat(nextPositions, hasSize(4));
+        assertThat(nextPositions, hasItems("B6", "C5", "C3", "B2"));
+    }
+
+    @Test
+    public void rookCanMoveHorizontallyOrVertically() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.ROOK, "D5");
+
+        assertThat(nextPositions, hasSize(4));
+        assertThat(nextPositions, hasItems("A5", "H5", "D8", "D1"));
+    }
+
+    @Test
+    public void rookCanMoveHorizontallyOrVerticallyFromCorner() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.ROOK, "A1");
+
+        assertThat(nextPositions, hasSize(2));
+        assertThat(nextPositions, hasItems("A8", "H1"));
+    }
+
+    @Test
+    public void bishopCanMoveDiagonally() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.BISHOP, "D4");
+
+        assertThat(nextPositions, hasSize(4));
+        assertThat(nextPositions, hasItems("A7", "G1", "H8", "A1"));
+    }
+
+    @Test
+    public void bishopCanMoveDiagonallyIfPossible() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.BISHOP, "A6");
+
+        assertThat(nextPositions, hasSize(2));
+        assertThat(nextPositions, hasItems("C8", "F1"));
+    }
+
+    @Test
+    public void queenCanMoveInAllDirections() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.QUEEN, "D5");
+
+        assertThat(nextPositions, hasSize(8));
+        assertThat(nextPositions, hasItems("D8", "H5", "D1", "A5", "G8", "H1", "A2", "A8"));
+    }
+
+    @Test
+    public void queenCanMoveInAllDirectionsIfPossible() {
+        final List<String> nextPositions = chess.findAllNextPositions(Chess.Piece.QUEEN, "A2");
+
+        assertThat(nextPositions, hasSize(5));
+        assertThat(nextPositions, hasItems("A1", "H2", "A8", "B1", "G8"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationOfMoreThan2Length() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "A2A");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationOfNull() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationIncomplete() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "A");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationNonNumber() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "AA");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationBeforeA() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "x1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationBeyondH() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "I2");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationBelow1Row() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "A0");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailForIncorrectLocationAbove8Row() {
+        chess.findAllNextPositions(Chess.Piece.PAWN, "A9");
+    }
+}
